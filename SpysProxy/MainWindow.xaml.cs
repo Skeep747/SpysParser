8c24e1a006e077bd4d6601c2d97781d465c2c40d
Skeep <Skeep747@gmail.com>
2018-09-03 14:02:54 +0300

    done dev

---
+++ SpysProxy/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using SpysProxy.Infrastructure;
+
+namespace SpysProxy
+{
+    public partial class MainWindow
+    {
+        private Parser _parser;
+        private readonly ObservableCollection<LogItem> _dataItemsLog = new ObservableCollection<LogItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataGridLog.ItemsSource = _dataItemsLog;
+        }
+
+        private async void OnLogResult(LogItem logItem)
+        {
+            await Application.Current.Dispatcher.BeginInvoke(new Action(() => _dataItemsLog.Insert(0, logItem)));
+            if (logItem.Result != "Сканирование остановлено.") return;
+            await Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                StartButton.IsEnabled = true;
+            }));
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _parser = new Parser();
+            _parser.OnLogResult += OnLogResult;
+            _parser.Stop = false;
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            Task.Factory.StartNew(_parser.AllCountrys);
+        }
+
+        private void StopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _parser.Stop = true;
+            StopButton.IsEnabled = false;
+        }
+
+        private void LaunchSpysParserOnGitHub(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Skeep747/SpysParser");
+        }
+    }
+}
